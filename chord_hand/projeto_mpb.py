7d81add189cae824554a8eac023cde7e0b2ba080
jiraffarij <felipe.martins4@hotmail.com>
2024-10-01 16:57:06 -0300

    Correct naming

--- chord_hand/projeto_mpb.py
+++ chord_hand/projeto_mpb.py
@@ -2,8 +2,8 @@ import chord_hand.settings
 
 
 def analysis_to_projeto_mpb_code(analysis, modality):
-    key = analysis.type.name, analysis.step, analysis.chroma
-    qualities_to_codes = chord_hand.settings.analytic_type_args_to_projeto_mpb_code[modality].get(key, None)
+    analytic_type = analysis.type.name, analysis.step, analysis.chroma
+    qualities_to_codes = chord_hand.settings.analytic_type_args_to_projeto_mpb_code[modality].get(analytic_type, None)
     if not qualities_to_codes:
         return ''
     for quality_str, code in qualities_to_codes.items():
